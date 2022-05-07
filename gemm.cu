ce98dcaa04bd51b41a9ebe9a7aba924b067c9739
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2022-05-07 01:39:45 -0500

    remove FP16 and switch to complex numbers

--- gemm.cu
+++ gemm.cu
@@ -8,40 +8,48 @@
 
 using namespace std;
 
-// #define FP16MM
 
-const char* cublasGetErrorString(cublasStatus_t status)
+
+const char *cublasGetErrorString(cublasStatus_t status)
 {
-    switch(status)
-    {
-        case CUBLAS_STATUS_SUCCESS: return "CUBLAS_STATUS_SUCCESS";
-        case CUBLAS_STATUS_NOT_INITIALIZED: return "CUBLAS_STATUS_NOT_INITIALIZED";
-        case CUBLAS_STATUS_ALLOC_FAILED: return "CUBLAS_STATUS_ALLOC_FAILED";
-        case CUBLAS_STATUS_INVALID_VALUE: return "CUBLAS_STATUS_INVALID_VALUE"; 
-        case CUBLAS_STATUS_ARCH_MISMATCH: return "CUBLAS_STATUS_ARCH_MISMATCH"; 
-        case CUBLAS_STATUS_MAPPING_ERROR: return "CUBLAS_STATUS_MAPPING_ERROR";
-        case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED"; 
-        case CUBLAS_STATUS_INTERNAL_ERROR: return "CUBLAS_STATUS_INTERNAL_ERROR"; 
-    }
-    return "unknown error";
+  switch (status)
+  {
+  case CUBLAS_STATUS_SUCCESS:
+    return "CUBLAS_STATUS_SUCCESS";
+  case CUBLAS_STATUS_NOT_INITIALIZED:
+    return "CUBLAS_STATUS_NOT_INITIALIZED";
+  case CUBLAS_STATUS_ALLOC_FAILED:
+    return "CUBLAS_STATUS_ALLOC_FAILED";
+  case CUBLAS_STATUS_INVALID_VALUE:
+    return "CUBLAS_STATUS_INVALID_VALUE";
+  case CUBLAS_STATUS_ARCH_MISMATCH:
+    return "CUBLAS_STATUS_ARCH_MISMATCH";
+  case CUBLAS_STATUS_MAPPING_ERROR:
+    return "CUBLAS_STATUS_MAPPING_ERROR";
+  case CUBLAS_STATUS_EXECUTION_FAILED:
+    return "CUBLAS_STATUS_EXECUTION_FAILED";
+  case CUBLAS_STATUS_INTERNAL_ERROR:
+    return "CUBLAS_STATUS_INTERNAL_ERROR";
+  }
+  return "unknown error";
 }
 
 // Convenience function for checking CUDA runtime API results
 // can be wrapped around any runtime API call. No-op in release builds.
-inline
-cudaError_t checkCuda(cudaError_t result)
+inline cudaError_t checkCuda(cudaError_t result)
 {
-  if (result != cudaSuccess) {
+  if (result != cudaSuccess)
+  {
     fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
     assert(result == cudaSuccess);
   }
   return result;
 }
 
-inline
-cublasStatus_t checkCublas(cublasStatus_t result)
+inline cublasStatus_t checkCublas(cublasStatus_t result)
 {
-  if (result != CUBLAS_STATUS_SUCCESS) {
+  if (result != CUBLAS_STATUS_SUCCESS)
+  {
     fprintf(stderr, "CUDA Runtime Error: %s\n", cublasGetErrorString(result));
     assert(result == CUBLAS_STATUS_SUCCESS);
   }
@@ -49,130 +57,106 @@ cublasStatus_t checkCublas(cublasStatus_t result)
 }
 
 // Fill the array A(nr_rows_A, nr_cols_A) with random numbers on CPU
-void CPU_fill_rand(float *A, int nr_rows_A, int nr_cols_A) {
-	int a=1;
+void CPU_fill_rand(cuComplex *A, int nr_rows_A, int nr_cols_A)
+{
+  int a = 1;
 
-    for(int i = 0; i < nr_rows_A * nr_cols_A; i++){
-		A[i] = (float)rand()/(float)(RAND_MAX/a);
-	}
+  for (int i = 0; i < nr_rows_A * nr_cols_A; i++)
+  {
+    A[i] = {(float)rand() / (float)(RAND_MAX / a), 0};
+  }
 }
 
-int main(int argc, char ** argv){
-
+int main(int argc, char **argv)
+{
 
   int min_m_k_n = 2;
-  int max_m_k_n = 4096*4;
+  int max_m_k_n = 4096 * 4;
   int repeats = 2;
   int verbose = 1;
 
-#ifndef FP16MM
-  cout << "\ncublasSgemm test result:\n" << endl;
-#else
-  cout << "\ncublasHgemm test result:\n" << endl;
-#endif
-  
-  if(verbose) 
-    cout << "running with" 
-	 << " min_m_k_n: " << min_m_k_n
-	 << " max_m_k_n: " << max_m_k_n
-	 << " repeats: " << repeats
-	 << endl;
+  cout << "\ncublasCgemm test result:\n"
+       << endl;
+
+  if (verbose)
+    cout << "running with"
+         << " min_m_k_n: " << min_m_k_n
+         << " max_m_k_n: " << max_m_k_n
+         << " repeats: " << repeats
+         << endl;
 
   cublasStatus_t stat;
   cublasHandle_t handle;
 
   checkCublas(cublasCreate(&handle));
 
-  if(verbose) cout << "allocating device variables" << endl;
-  
+  if (verbose)
+    cout << "allocating device variables" << endl;
+
   // Allocate 3 arrays on CPU
-  
-  float *h_A = (float *)malloc(max_m_k_n * max_m_k_n * sizeof(float));
-  float *h_B = (float *)malloc(max_m_k_n * max_m_k_n * sizeof(float));
-  float *h_C = (float *)malloc(max_m_k_n * max_m_k_n * sizeof(float));
-  
+
+  cuComplex *h_A = (cuComplex *)malloc(max_m_k_n * max_m_k_n * sizeof(cuComplex));
+  cuComplex *h_B = (cuComplex *)malloc(max_m_k_n * max_m_k_n * sizeof(cuComplex));
+  cuComplex *h_C = (cuComplex *)malloc(max_m_k_n * max_m_k_n * sizeof(cuComplex));
+
   CPU_fill_rand(h_A, max_m_k_n, max_m_k_n);
   CPU_fill_rand(h_B, max_m_k_n, max_m_k_n);
   CPU_fill_rand(h_C, max_m_k_n, max_m_k_n);
 
-#ifndef FP16MM
-    // Allocate 3 arrays on GPU
-    float *d_A, *d_B, *d_C;
-    checkCuda(cudaMallocManaged(&d_A, max_m_k_n * max_m_k_n * sizeof(float)));
-    checkCuda(cudaMallocManaged(&d_B, max_m_k_n * max_m_k_n * sizeof(float)));
-    checkCuda(cudaMallocManaged(&d_C, max_m_k_n * max_m_k_n * sizeof(float)));
-    
-    checkCuda(cudaMemcpy(d_A,h_A,max_m_k_n * max_m_k_n * sizeof(float),cudaMemcpyHostToDevice));
-    checkCuda(cudaMemcpy(d_B,h_B,max_m_k_n * max_m_k_n * sizeof(float),cudaMemcpyHostToDevice));
-    checkCuda(cudaMemcpy(d_C,h_C,max_m_k_n * max_m_k_n * sizeof(float),cudaMemcpyHostToDevice));
-    
-    int lda, ldb, ldc, m, n, k;
-    const float alf = 1.0f;
-    const float bet = 0.0f;
-    const float *alpha = &alf;
-    const float *beta = &bet;
-  
-#else
-    
-  	__half *d_A, *d_B, *d_C;
-    checkCuda(cudaMallocManaged(&d_A, max_m_k_n * max_m_k_n * sizeof(__half)));
-    checkCuda(cudaMallocManaged(&d_B, max_m_k_n * max_m_k_n * sizeof(__half)));
-    checkCuda(cudaMallocManaged(&d_C, max_m_k_n * max_m_k_n * sizeof(__half)));
-    
-    for (int i = 0; i < max_m_k_n * max_m_k_n; i++) {
-      d_A[i] = approx_float_to_half(h_A[i]);
-  	  d_B[i] = approx_float_to_half(h_B[i]);
-  	  d_C[i] = approx_float_to_half(h_C[i]);
-    }
-    
-    int lda, ldb, ldc, m, n, k;
-    const __half alf = approx_float_to_half(1.0);
-    const __half bet = approx_float_to_half(0.0);
-    const __half *alpha = &alf;
-    const __half *beta = &bet;
-	
-#endif
-  
+  // Allocate 3 arrays on GPU
+  cuComplex *d_A, *d_B, *d_C;
+  checkCuda(cudaMallocManaged(&d_A, max_m_k_n * max_m_k_n * sizeof(cuComplex)));
+  checkCuda(cudaMallocManaged(&d_B, max_m_k_n * max_m_k_n * sizeof(cuComplex)));
+  checkCuda(cudaMallocManaged(&d_C, max_m_k_n * max_m_k_n * sizeof(cuComplex)));
+
+  checkCuda(cudaMemcpy(d_A, h_A, max_m_k_n * max_m_k_n * sizeof(cuComplex), cudaMemcpyHostToDevice));
+  checkCuda(cudaMemcpy(d_B, h_B, max_m_k_n * max_m_k_n * sizeof(cuComplex), cudaMemcpyHostToDevice));
+  checkCuda(cudaMemcpy(d_C, h_C, max_m_k_n * max_m_k_n * sizeof(cuComplex), cudaMemcpyHostToDevice));
+
+  int lda, ldb, ldc, m, n, k;
+  const cuComplex alf = {1.0f, 0};
+  const cuComplex bet = {0.0f, 0};
+  const cuComplex *alpha = &alf;
+  const cuComplex *beta = &bet;
+
   cudaEvent_t start, stop;
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
 
-  for(int size = min_m_k_n; size <= max_m_k_n; size=size*2){
+  for (int size = min_m_k_n; size <= max_m_k_n; size = size * 2)
+  {
     double sum = 0.0;
-    for(int rep = 0; rep < repeats; rep++){
+    for (int rep = 0; rep < repeats; rep++)
+    {
       cudaEventRecord(start, 0);
-	  m=n=k=size;
-	  lda = m;
-	  ldb = k;
-	  ldc = m;
-#ifndef FP16MM
-        stat = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, d_A, lda, d_B, ldb, beta, d_C, ldc); 
-#else
-	stat = cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, d_A, lda, d_B, ldb, beta, d_C, ldc); 
-#endif
-      cudaEventRecord(stop,0);
+      m = n = k = size;
+      lda = m;
+      ldb = k;
+      ldc = m;
+
+      stat = cublasCgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, d_A, lda, d_B, ldb, beta, d_C, ldc);
+
+      cudaEventRecord(stop, 0);
       cudaEventSynchronize(stop);
-      if(stat != CUBLAS_STATUS_SUCCESS){
-	cerr << "cublasSgemmBatched failed" << endl;
-	exit(1);
+      if (stat != CUBLAS_STATUS_SUCCESS)
+      {
+        cerr << "cublasSgemmBatched failed" << endl;
+        exit(1);
       }
       assert(!cudaGetLastError());
-      
+
       float elapsed;
       cudaEventElapsedTime(&elapsed, start, stop);
       elapsed /= 1000.0f;
       sum += elapsed;
     }
-#ifndef FP16MM	
-  cout << "float32: size " 
-#else
-  cout << "float16: size " 
-#endif
-  << size << " average: " << sum/repeats << " s "<< endl;
 
+    cout << "complex32: size "
+
+         << size << " average: " << sum / repeats << " s " << endl;
   }
 
-  //Free GPU memory
+  // Free GPU memory
   cudaFree(d_A);
   cudaFree(d_B);
   cudaFree(d_C);
@@ -181,6 +165,6 @@ int main(int argc, char ** argv){
   free(h_A);
   free(h_B);
   free(h_C);
-      
+
   return 0;
 }
